693bd632a10e171e2c6bd562e2993d6bbf6c4042
HAIYIN <1483723999@qq.com>
2021-09-08 15:40:08 +0800

    项目初始化

---
+++ mall-hotel/src/main/java/com/zhyxyb/mallhotel/MallHotelApplication.java
@@ -0,0 +1,13 @@
+package com.zhyxyb.mallhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallHotelApplication.class, args);
+    }
+
+}
---
+++ mall-hotel/src/test/java/com/zhyxyb/mallhotel/MallHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhyxyb.mallhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
